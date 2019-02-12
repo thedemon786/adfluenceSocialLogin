aab0109f9bb4f957528f65aa832fad31c2d885a3
Owais Malik <owaismalik@outlook.com>
2019-02-12 15:08:44 +0530

    first change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        If(env('APP_ENV') !== 'local') { $url->forceSchema('https'); }
+        $url->forceSchema('https');
 
 
         //
